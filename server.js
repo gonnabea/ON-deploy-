10657fb408d4eb90178dfad175ffff54a91d40ed
Choi Jiwon <winning1993@naver.com>
2020-11-09 02:17:33 +0900

    [deploy] 배포를 위한 환경설정

--- server.js
+++ server.js
@@ -113,7 +113,37 @@ app.get("/deletepost/:id", (req, res) => {
 
 //////////////////////////////////////// 순수 SQL 테스트 ///////////////////////////////////////////////
 
-app.use(cors())
+// CORS 오류 해결법
+
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:5000",
+  "https://shrouded-journey-38552.heroku...",
+]
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+}
+
+app.use(cors(corsOptions))
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")))
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
+  })
+} // 헤로쿠에 리액트 프론트를 배포하기 위함
+
 app.use(helmet())
 app.use(express.static("public"))
 app.use(cookieParser())
