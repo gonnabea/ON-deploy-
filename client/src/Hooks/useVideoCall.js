9c10700c1b9c9636a1b41f6bf97f90f9cb4cb7cb
Choi Jiwon <winning1993@naver.com>
2020-11-16 16:17:35 +0900

    [ADD] peerjs p2p 연결 (15)

--- client/src/Hooks/useVideoCall.js
+++ client/src/Hooks/useVideoCall.js
@@ -35,7 +35,7 @@ const useVideoCall = () => {
     const video = document.createElement("video")
     const videoGrid = document.getElementById("videoGrid")
     const videoStream = await navigator.mediaDevices.getUserMedia({
-      video: { width: 180, height: 120 },
+      video: { width: 360, height: 240 },
       audio: true,
       echoCancellation: true,
     })
@@ -57,7 +57,7 @@ const useVideoCall = () => {
         connection.send("hi!")
         console.log("유저가 접속해서 컨넥션 오픈됨, 상대에게 hi라고 보냄")
       })
-      const call = peer.call(id, streamForSending())
+      const call = peer.call(id, videoStream)
       console.log(call)
       console.log(connection)
     })
@@ -70,7 +70,7 @@ const useVideoCall = () => {
     })
 
     peer.on("call", (call) => {
-      call.answer(streamForSending())
+      call.answer(videoStream)
 
       call.on("stream", (stream) => {
         console.dir(stream)
