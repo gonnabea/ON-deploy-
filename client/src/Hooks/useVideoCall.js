ff7166e541fd5294341bfffaa9a39053d102333d
Choi Jiwon <winning1993@naver.com>
2020-11-18 09:18:14 +0900

    [ADD] 화상채팅 연결 시 내 비디오 뮤트하기

--- client/src/Hooks/useVideoCall.js
+++ client/src/Hooks/useVideoCall.js
@@ -29,16 +29,17 @@ const useVideoCall = () => {
       audio: true,
       echoCancellation: true,
     })
+
     video.srcObject = videoStream
     video.addEventListener("loadedmetadata", () => {
       video.play()
       videoGrid.append(video)
     })
 
-    peersConnection(videoStream)
+    peersConnection(videoStream, video)
   }
 
-  const peersConnection = async (videoStream) => {
+  const peersConnection = async (videoStream, myVideo) => {
     // host와 port를 설정해주어 개인 peerjs 서버를 가동
     peer = new Peer(await getLoggedUser())
     peerList.current.myPeer = peer.id
@@ -76,6 +77,7 @@ const useVideoCall = () => {
       console.log(callConn)
       const video = document.createElement("video")
       callConn.on("stream", (userVideoStream) => {
+        myVideo.muted = true
         const videoGrid = document.getElementById("videoGrid")
         video.srcObject = userVideoStream
         video.addEventListener("loadedmetadata", () => {
