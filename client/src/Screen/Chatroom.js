3f5fb4720572b5b4cc74dee6d669186b7a2a627d
Choi Jiwon <winning1993@naver.com>
2020-11-19 02:09:24 +0900

    [UPDATE] 개인 peerjs 서버 적용 (클라이언트 수정 필요?)

--- client/src/Screen/Chatroom.js
+++ client/src/Screen/Chatroom.js
@@ -134,7 +134,13 @@ const Chatroom = () => {
 
     const peersConnection = async (videoStream, myVideo) => {
       // host와 port를 설정해주어 개인 peerjs 서버를 가동
-      peer = new Peer(loggedUser.id)
+      peer = new Peer(loggedUser.id, {
+        secure: true,
+        host: "our-now/herokuapp.com",
+        path: "/",
+        key: "peerjs",
+        debug: true,
+      })
       peerList.current.myPeer = peer.id
       console.log(peer)
 
