db169d669edaa184b874587fff3d2741c7ab1c4d
Choi Jiwon <winning1993@naver.com>
2021-03-30 03:01:30 +0900

    [FIX] 네비게이션 버그 수정

--- client/src/Hooks/useNavigation.js
+++ client/src/Hooks/useNavigation.js
@@ -61,55 +61,49 @@ const Navigation = () => {
 
   const getLoggedUser = async () => {
     const userData = await api.getLoggedUser()
-    const User = await userData.data
+
+    const User = userData.data
     setUser(User)
   }
 
-  const renderNav = async () => {
-    console.log("rendered!")
-    await getLoggedUser()
-    if (isLoading === false) {
-      return user ? (
-        <Header>
-          <SLink to="/">홈으로</SLink>
-          <SLink to="/chatroom/lobby">채팅</SLink>
-          <SLink to="/setting">설정</SLink>
-          <Form action="https://our-now.herokuapp.com/logout" method="post">
-            <Input type="submit" value="로그아웃" />
-          </Form>
-        </Header>
-      ) : (
-        <Container>
-          <Header>
-            <SLink to="/">홈으로</SLink>
-            <SLink to="/chatroom/lobby">채팅</SLink>
-          </Header>
-          <MenuTitle>로그인</MenuTitle>
-          <Form action="https://our-now.herokuapp.com/login" method="post">
-            <Input type="text" name="username" placeholder="username" required={true} />
-            <Input type="password" name="password" placeholder="password" required={true} />
-            <Submit type="submit" value="Login" />
-          </Form>
-          <MenuTitle>회원가입</MenuTitle>
-          <Form action="https://our-now.herokuapp.com/join" method="post">
-            <Input type="email" name="email" placeholder="E-mail" required={true} />
-            <Input type="text" name="username" placeholder="Username" required={true} />
-            <Input type="password" name="password" placeholder="Password" required={true} />
-            <Input type="password" name="password2" placeholder="Verify Password" required={true} />
-            <Submit type="submit" value="Join" />
-          </Form>
-        </Container>
-      )
-    } else {
-      return <Loader />
-    }
-  }
-
   useEffect(() => {
+    getLoggedUser()
     setLoading(false)
   }, [])
 
-  return <Container>{renderNav()}</Container>
+  return user ? (
+    <Container>
+      <Header>
+        <SLink to="/">홈으로</SLink>
+        <SLink to="/chatroom/lobby">채팅</SLink>
+        <SLink to="/setting">설정</SLink>
+        <Form action="https://our-now.herokuapp.com/logout" method="post">
+          <Input type="submit" value="로그아웃" />
+        </Form>
+      </Header>
+    </Container>
+  ) : (
+    <Container>
+      <Header>
+        <SLink to="/">홈으로</SLink>
+        <SLink to="/chatroom/lobby">채팅</SLink>
+      </Header>
+      <MenuTitle>로그인</MenuTitle>
+      <Form action="https://our-now.herokuapp.com/login" method="post">
+        <Input type="text" name="username" placeholder="username" required={true} />
+        <Input type="password" name="password" placeholder="password" required={true} />
+        <Submit type="submit" value="Login" />
+      </Form>
+      <MenuTitle>회원가입</MenuTitle>
+      <Form action="https://our-now.herokuapp.com/join" method="post">
+        <Input type="email" name="email" placeholder="E-mail" required={true} />
+        <Input type="text" name="username" placeholder="Username" required={true} />
+        <Input type="password" name="password" placeholder="Password" required={true} />
+        <Input type="password" name="password2" placeholder="Verify Password" required={true} />
+        <Submit type="submit" value="Join" />
+      </Form>
+    </Container>
+  )
 }
 
 export default Navigation
