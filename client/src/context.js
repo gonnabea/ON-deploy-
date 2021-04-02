3a68d1cd7c3e805f014d9adcf6ad6f0d542b6d29
Choi Jiwon <winning1993@naver.com>
2021-04-03 03:36:47 +0900

    [ADD] Context-API 도입중..

--- client/src/context.js
+++ client/src/context.js
@@ -1,16 +1,20 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import api from "./api"
 
 export const UserContext = React.createContext()
 
-let loggedUser
-const getLoggedUser = async () => {
-  loggedUser = await api.getLoggedUser()
-  loggedUser = loggedUser.data
+const UserContextProvider = ({ children }) => {
+  const [loggedUser, setLoggedUser] = useState()
+  const getLoggedUser = async () => {
+    const { data } = await api.getLoggedUser()
+    setLoggedUser(data)
+  }
+
+  useEffect(() => {
+    getLoggedUser()
+  })
+
+  return <UserContext.Provider value={loggedUser}>{children}</UserContext.Provider>
 }
-getLoggedUser()
-const UserContextProvider = ({ children }) => (
-  <UserContext.Provider value={loggedUser}>{children}</UserContext.Provider>
-)
 
 export default UserContextProvider
