506cd3b4c755f1788032a8a2a9f1d3121a6d884f
Choi Jiwon <winning1993@naver.com>
2021-04-03 02:53:26 +0900

    [ADD] Context-API 도입중..

--- client/src/context.js
+++ client/src/context.js
@@ -4,7 +4,7 @@ import api from "./api"
 export const UserContext = React.createContext()
 
 const UserContextProvider = async ({ children }) => (
-  <UserContext.Provider value={await api.getLoggedUser()}>{children}</UserContext.Provider>
+  <UserContext.Provider value={{ name: "Jiwon" }}>{children}</UserContext.Provider>
 )
 
 export default UserContextProvider
