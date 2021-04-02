8dd6e95336ee3eb3e24a116edd89e87436a577a0
Choi Jiwon <winning1993@naver.com>
2021-04-03 07:09:55 +0900

    [ADD] Context-API 로 User Data 이용중 ...

--- client/src/userContext.js
+++ client/src/userContext.js
@@ -4,7 +4,7 @@ import api from "./api"
 export const UserContext = React.createContext()
 
 const UserContextProvider = ({ children }) => {
-  const [loggedUser, setLoggedUser] = useState("")
+  const [loggedUser, setLoggedUser] = useState(null)
   const [isLoading, setLoading] = useState(true)
   const getLoggedUser = async () => {
     const { data } = await api.getLoggedUser()
@@ -17,7 +17,7 @@ const UserContextProvider = ({ children }) => {
   }, [])
 
   return isLoading ? (
-    <UserContext.Provider value={"loading..."}>{children}</UserContext.Provider>
+    <UserContext.Provider value={null}>{children}</UserContext.Provider>
   ) : (
     <UserContext.Provider value={loggedUser}>{children}</UserContext.Provider>
   )
